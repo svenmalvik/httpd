29d154da4a2584fc132a223ef556822e9c0a53b6
Sven Malvik <Sven.Malvik@husbanken.no>
2016-10-07 12:37:18 +0200

    2.4

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-consul-template -consul=$CONSUL -log-level debug --once -template "/etc/consul-templates/app.conf.tmpl:/usr/local/apache2/conf/extra/app.conf:apachectl -k restart"
+consul-template -consul=$CONSUL -log-level debug -template "/etc/consul-templates/app.conf.tmpl:/usr/local/apache2/conf/extra/app.conf:apachectl -k restart"
